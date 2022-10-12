d786ff7f0f535658809b78e4f4d656634c8140a9
carolpandolfe <carol.pandolfe@gmail.com>
2022-10-12 12:37:17 -0300

    Create Text input component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,17 +11,22 @@ module.exports = {
         xl: 24,
         "2xl": 32,
       },
+      transparent: "transparent",
       black: "#000",
       white: "#FFF",
 
-      "gray-100": "#E1E1E6",
-      "gray-200": "#C4C4CC",
-      "gray-400": "#7C7C8A",
-      "gray-800": "#202024",
-      "gray-900": "#121214",
+      gray: {
+        100: "#E1E1E6",
+        200: "#C4C4CC",
+        400: "#7C7C8A",
+        800: "#202024",
+        900: "#121214",
+      },
 
-      "cyan-300": "#9BE1FB",
-      "cyan-500": "#81D8F7",
+      cyan: {
+        300: "#9BE1FB",
+        500: "#81D8F7",
+      },
     },
     extend: {
       fontFamily: {
